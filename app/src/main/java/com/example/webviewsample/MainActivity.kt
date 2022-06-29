d937133a4d0fc7e1f7d936a3ee8d9771f7334ea0
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-29 22:47:43 +0400

    feat(app): add 'MainActivity'

--- app/src/main/java/com/example/webviewsample/MainActivity.kt
+++ app/src/main/java/com/example/webviewsample/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.webviewsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.webviewsample.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val webSettings = binding.webView.settings
+        @SuppressLint("SetJavaScriptEnabled")
+        webSettings.javaScriptEnabled = true
+
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                view.loadUrl(request.url.toString())
+                return true
+            }
+        }
+
+        binding.webView.loadUrl(getString(R.string.BASIC_URL))
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && binding.webView.canGoBack()) {
+            binding.webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
